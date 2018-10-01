5fa02002223b233eefede74893d28da12146b5e5
Sean Mulvenna <smulvenna@gcdtech.com>
2018-10-01 10:37:35 +0100

    First round of changes

--- src/AddressUkPafLookupView.php
+++ src/AddressUkPafLookupView.php
@@ -54,28 +54,59 @@ class AddressUkPafLookupView extends ControlView
         $country->setSelectionItems([["", "Please select..."], $countriesList]);
         $postCodeSearch->setPlaceholderText("Postcode");
         $houseNumber->setPlaceholderText("No.");
+
+        $country->addCssClassNames();
     }
 
     public function printViewContent()
     {
-        $this->layoutItemsWithContainer("", ["Country"]);
         ?>
-        <div class="search-fields">
-            <div class="search-results">
-                <span class="search-results-msg"></span>
-                <ul class="search-results-items"></ul>
-            </div>
+
+        <label class="c-label">Country <span class="u-negative">*</span></label>
+        <div class="c-select-box u-marg-bottom">
             <?php
-            $this->layoutItemsWithContainer("", [
-                "Find Address" => "{HouseNumber}{PostCodeSearch}{Search}<span class='spinner'></span>"
-            ]);
+            $this->leaves['Country']->addCssClassNames('c-text-input');
+            print $this->leaves['Country'];
             ?>
-            <span class="search-error">Insert a valid Post Code</span>
         </div>
 
-        <p class="manual-address-par _help">Don't know the postcode? <a class="manual-address-link" href='#'>enter their
-                address manually</a>.</p>
-        <p class="search-address-link _help"><b><a href='#'>Search again</a></b></p>
+        <div class="search-fields u-pos-rel">
+            <div class="search-results c-module c-dropdown c-dropdown--paf u-bordered u-fill-white u-shadow-light">
+
+                <ul class="search-results-items u-micro"></ul>
+            </div>
+
+            <?php
+
+            print '<label class="c-label">Address Lookup <span class="u-negative">*</span></label>';
+
+            ?>
+
+            <div class="u-pos-rel">
+                <div class="o-flex o-flex--align-middle o-flex--flex-start">
+                    <?php
+                    $this->leaves['HouseNumber']->addCssClassNames('c-text-input c-text-input--very-short u-marg-right-half');
+                    print $this->leaves['HouseNumber'];
+                    ?>
+                    <div class="o-flex o-flex--align-middle o-flex__item o-flex--flex-end dont-grow">
+                        <?php
+                        $this->leaves['PostCodeSearch']->addCssClassNames('c-text-input c-text-input--short');
+                        print $this->leaves['PostCodeSearch'];
+
+                        $this->leaves['Search']->addCssClassNames('c-button c-text-input-button');
+                        print $this->leaves['Search'];
+                        ?>
+                        <span class='spinner'></span>
+                    </div>
+                </div>
+            </div>
+
+            <span class="js-validation-message search-error u-marg-bottom"><p>Insert a valid Post Code</p></span>
+
+        </div>
+
+        <p class="manual-address-par _help u-marg-top">Don't know the postcode? <a class="manual-address-link" href='#'>enter their address manually</a>.</p>
+
         <div class="manual-fields">
             <?php
 
@@ -86,15 +117,30 @@ class AddressUkPafLookupView extends ControlView
                 $postcodeLabel = "Zip Code";
             }
 
-            $this->layoutItemsWithContainer("", [
-                "Address Line 1" => "Line1",
-                "Address Line 2" => "Line2",
-                "Town",
-                $countyLabel => "County",
-                $postcodeLabel => "Postcode"
-            ]);
+            print '<label class="c-label">Address Line 1 <span class="u-negative">*</span></label>';
+            $this->leaves['Line1']->addCssClassNames('c-text-input c-text-input--reg');
+            print $this->leaves['Line1'];
+
+            print '<label class="c-label">Address Line 2 <span class="u-negative">*</span></label>';
+            $this->leaves['Line2']->addCssClassNames('c-text-input c-text-input--reg');
+            print $this->leaves['Line2'];
+
+            print '<label class="c-label">Town <span class="u-negative">*</span></label>';
+            $this->leaves['Town']->addCssClassNames('c-text-input c-text-input--reg');
+            print $this->leaves['Town'];
+
+            print '<label class="c-label"> '.$countyLabel.' <span class="u-negative">*</span></label>';
+            $this->leaves['County']->addCssClassNames('c-text-input c-text-input--reg');
+            print $this->leaves['County'];
+
+            print '<label class="c-label">'.$postcodeLabel.' <span class="u-negative">*</span></label>';
+            $this->leaves['Postcode']->addCssClassNames('c-text-input c-text-input--short');
+            print $this->leaves['Postcode'];
+
             ?>
         </div>
+
+        <p class="search-address-link _help u-marg-top"><b><a href='#'>Search again</a></b></p>
         <?php
     }
 }
